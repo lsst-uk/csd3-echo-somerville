5f26dac69b9ff1911fb4dda5641678dfd69fdbbe
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-02-23 17:08:58 +0000

    progress on boto3

--- csd3-side/bucket_manager.py
+++ csd3-side/bucket_manager.py
@@ -5,14 +5,14 @@ Helper functions for using an s3 bucket
 """
 import boto3
 import json
-import os
+import os, sys
 import warnings
-
-with warnings.catch_warnings():
-    warnings.filterwarnings('ignore')
+from botocore.exceptions import ClientError
 
 def print_buckets(client):
-    response = client.list_buckets()
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore')
+        response = client.list_buckets()
     for bucket in response['Buckets']:
         print(f"{bucket['Name']}\t{bucket['CreationDate']}")
 
@@ -33,8 +33,26 @@ def get_client(access_key, secret_key, host):
     )
 
 def bucket_list(client):
-    response = client.list_buckets()
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore')
+        response = client.list_buckets()
     return [ b['Name'] for b in response['Buckets'] ]
 
 def create_bucket(client,bucket_name):
-    return client.create_bucket(Bucket=bucket_name)
+    try:
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore')
+            client.create_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        print(f'ClientError {e}')
+        return False
+    except Error as e:
+        print(f'Error {e}')
+        return False
+    return True
+
+def object_list(client,bucket_name):
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore')
+        response = client.list_objects(Bucket=bucket_name)
+    return [ c['Key'] for c in response['Contents'] ]
