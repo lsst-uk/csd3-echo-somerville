87a2c710aef11a57b77dfd38670a9e86626faea7
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-03-21 09:22:40 +0000

    bucket_size.py done

--- scripts/bucket_size.py
+++ scripts/bucket_size.py
@@ -21,14 +21,25 @@ s3 = bm.get_resource(access_key,secret_key,s3_host)
 
 bucket_name = sys.argv[1]
 bucket = s3.Bucket(bucket_name)
-
+#num_objects = sum(1 for _ in bucket.objects.all())
+#print(num_objects)
+total_size = 0
+i=0
 try:
-    print('Calculating bucket size...')
-    sizes = tqdm([ob.size for ob in bucket.objects.all()], total=len(bucket.objects.all()))
+    print('Calculating bucket size (".": 1000 objects)')
+    # with tqdm(total=sum(1 for _ in bucket.objects.all())) as pbar:
+    for ob in bucket.objects.all():
+        total_size += ob.size
+        if i%1000==0:
+            print('.', end='', flush=True)
+        i+=1
+    print()
+            # pbar.update(1)
+    #sizes = [ob.size for ob in tqdm(bucket.objects.all(), total=sum(1 for _ in bucket.objects.all()))]
 except Exception as e:
     if '(NoSuchBucket)' in str(e).split():
         print('NoSuchBucket')
 
-total_size = sum(sizes)
+# total_size = sum(sizes)
 
 print(f'Total size of bucket: {total_size/1024**3:.2f} GiB')
