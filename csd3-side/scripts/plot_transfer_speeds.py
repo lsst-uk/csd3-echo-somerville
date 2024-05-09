ebde178ff8048ade9d959a2e7978d7d3b47231d6
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-05-09 15:36:01 +0100

    transfer speeds updated and moving to notebook

--- csd3-side/scripts/plot_transfer_speeds.py
+++ csd3-side/scripts/plot_transfer_speeds.py
@@ -5,11 +5,12 @@ if len(sys.argv) != 2:
     print(f'Usage: {sys.argv[0]} <logfile>')
     sys.exit(1)
 logfile = sys.argv[1]
+title_name = logfile.split("/")[-1].split(".")[0]
 # Define the regular expression pattern to match the desired lines
 pattern = 'uploaded in'  # Modified pattern to match both lines
 
 # Lists to store the extracted data
-mib_per_sec = []
+mbps = []
 sec_per_file = []
 
 with open(logfile, 'r') as lf:
@@ -19,7 +20,7 @@ with open(logfile, 'r') as lf:
             #3.01 MiB uploaded in 0.74 seconds, 4.09 MiB/s
             #2 files (avg 3.17 MiB/file) uploaded in 0.43 seconds, 0.22 s/file
             if 'MiB/s' in line:
-                mib_per_sec.append(float(line.split()[6]))
+                mbps.append(float(line.split()[6])*8)
             elif 's/file' in line:
                 sec_per_file.append(float(line.split()[9]))
 
@@ -28,15 +29,15 @@ with open(logfile, 'r') as lf:
 plt.figure(1)
 
 # Smooth out the data by calculating the moving average
-window_size = 1000
-smoothed_mib_per_sec = np.convolve(mib_per_sec, np.ones(window_size)/window_size, mode='valid')
+window_size = 10
+smoothed_mib_per_sec = np.convolve(mbps, np.ones(window_size)/window_size, mode='valid')
 smoothed_sec_per_file = np.convolve(sec_per_file, np.ones(window_size)/window_size, mode='valid')
-plt.suptitle(f'Transfer Speeds - Moving average over {window_size} folders')
+plt.suptitle(f'Transfer Speeds - Moving average over {window_size} folders.\n{title_name}')
 # Plot the smoothed data
 plt.subplot(211)
 plt.plot(smoothed_mib_per_sec)
 plt.xlabel('Folders')
-plt.ylabel('MiB/s')
+plt.ylabel('Mbit/s')
 
 plt.subplot(212)
 plt.plot(smoothed_sec_per_file)
@@ -44,4 +45,4 @@ plt.xlabel('Folders')
 plt.ylabel('s/file')
 plt.tight_layout()
 # Save the figure
-plt.savefig('transfer_speeds.png')
+plt.savefig(f'transfer_speeds_{title_name}.png')
