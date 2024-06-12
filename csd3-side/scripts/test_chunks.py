46238e5222538d927ed0be75719c36037783341d
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-06-12 11:32:58 +0100

    test_chunks.py - toy code for chunking list of folders to zip based on nprocs

--- csd3-side/scripts/test_chunks.py
+++ csd3-side/scripts/test_chunks.py
@@ -1,3 +1,4 @@
+from itertools import repeat
 import os
 import sys
 import zipfile
@@ -6,7 +7,7 @@ from multiprocessing import Pool
 def process_files(directory, nprocs):
     # Get a list of all subfolders
     subfolders = [f.path for f in os.scandir(directory) if f.is_dir()]
-    print(subfolders)
+    # print(subfolders)
     # Divide the list of subfolders into chunks
     chunks = [subfolders[i:i + nprocs] for i in range(0, len(subfolders), nprocs)]
     
@@ -15,23 +16,36 @@ def process_files(directory, nprocs):
 #     if underpopulate:
 # ...     b = [[x for y in a for x in y][i:i + 4] for i in range(0, len([x for y in a for x in y]), 4)]
 
-    print(chunks)
+    # print(chunks)
 
     # Use a multiprocessing pool to process each chunk
-    with Pool(nprocs) as p:
-        p.map(process_chunk, chunks)
+    p = Pool(nprocs)
+    results = p.map(process_chunk, zip(chunks, list(range(len(chunks)))))
+    p.close()
+    p.join()
+    return results
 
 def process_chunk(chunk):
     # Create a new zip file for this chunk
-    # with zipfile.ZipFile('archive.zip', 'w') as zipf:
-    #     # Add each file in the chunk to the zip file
-    #     for folder in chunk:
-    #         for root, dirs, files in os.walk(folder):
-    #             for file in files:
-    #                 zipf.write(os.path.join(root, file))
-    print(chunk)
+    chunk_folders, chunk_id = chunk
+    zipfile_name = f'zip_{chunk_id}.zip'
+    print(f'chunk {chunk}')
+    print(f'chunk_folders {chunk_folders}')
+    print(f'chunk_id {chunk_id}')
+    print(f'zipfile_name {zipfile_name}')
+    with zipfile.ZipFile(zipfile_name, 'w') as zipf:
+        # Add each file in the chunk to the zip file
+        for folder in chunk_folders:
+            for root, dirs, files in os.walk(folder):
+                for file in files:
+                    zipf.write(os.path.join(root, file))
+    print(f'{zipfile_name} done')
+    return str(chunk_folders), zipfile_name
 
 directory = sys.argv[1]
 nprocs = int(sys.argv[2])
 
-process_files(directory, nprocs)
+results = process_files(directory, nprocs)
+
+for result in results:
+    print(result)
