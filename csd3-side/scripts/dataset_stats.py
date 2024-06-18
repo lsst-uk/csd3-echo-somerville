f5b2369151d6c25d955dbcafd7a9fabfae003fe0
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-06-18 14:01:10 +0100

    fixed mean file size calc in dataset_stats

--- csd3-side/scripts/dataset_stats.py
+++ csd3-side/scripts/dataset_stats.py
@@ -10,17 +10,24 @@ def gather_stats(root):
     for dirname, dirs, files in os.walk(root):
         folder_count += 1
         local_file_count = 0
-        print(f"Processed {folder_count} folders.", end="\r", flush=True)
+        local_file_size = 0
+        # print(dirname)
+        # print(files)
+        # print(f"Processed {folder_count} folders.", end="\r", flush=True)
         for name in files:
             file_path = os.path.join(dirname, name)
             if os.path.isfile(file_path):
-                local_file_count += 1
                 file_count += 1
+                local_file_count += 1
                 total_file_size += os.path.getsize(file_path)
+                local_file_size += os.path.getsize(file_path)
+        print(local_file_size)
+        print(total_file_size)
         if local_file_count > 0:
-            if total_file_size / 1024**2 / local_file_count < 64:
+            print(local_file_size / 1024**2 / local_file_count)
+            if local_file_size / 1024**2 / local_file_count <= 64:
                 folders_with_mean_filesize_lt_64mb += 1
-        if local_file_count < 2:
+        if local_file_count <= 2:
             folders_with_lt_2_files += 1
     print()
     if file_count > 0:
@@ -55,10 +62,10 @@ def gather_stats(root):
     print(f"Total size of all files: {total_file_size/1024**2:.3f} MiB")
     print(f"Average file size: {average_file_size/1024**2:.3f} MiB")
     print(f"Average number of files per folder: {average_files_per_folder:.2f}")
-    print(f"Number of folders with mean file size < 64 MiB: {folders_with_mean_filesize_lt_64mb}")
-    print(f"Number of folders with < 2 files: {folders_with_lt_2_files}")
-    print(f"Percentage of folders with mean file size < 64 MiB: {perc_small_files:.2f} %")
-    print(f"Percentage of folders with < 2 files: {perc_low_file_count:.2f} %")
+    print(f"Number of folders with mean file size <= 64 MiB: {folders_with_mean_filesize_lt_64mb}")
+    print(f"Number of folders with <= 2 files: {folders_with_lt_2_files}")
+    print(f"Percentage of folders with mean file size <= 64 MiB: {perc_small_files:.2f} %")
+    print(f"Percentage of folders with <= 2 files: {perc_low_file_count:.2f} %")
     print(f"Collation of files during backup is advised if both percentages are high.\n\u2794  Here: {suggestion}")
 
 print(f"Gathering stats for folder: {sys.argv[1]}...")
