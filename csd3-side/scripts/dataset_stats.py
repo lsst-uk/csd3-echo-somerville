dbd26bba96887589ce908cdfeede789be459f662
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-06-18 14:33:08 +0100

    potential for collation stat added

--- csd3-side/scripts/dataset_stats.py
+++ csd3-side/scripts/dataset_stats.py
@@ -6,13 +6,15 @@ def gather_stats(root):
     total_file_size = 0
     folders_with_mean_filesize_lt_64mb = 0
     folders_with_lt_2_files = 0
+    potential_zip_count = 0
 
     for dirname, dirs, files in os.walk(root):
         folder_count += 1
         local_file_count = 0
         local_file_size = 0
-        # print(dirname)
+
         # print(files)
+        # print(dirs)
         print(f"Processed {folder_count} folders.", end="\r", flush=True)
         for name in files:
             file_path = os.path.join(dirname, name)
@@ -21,6 +23,8 @@ def gather_stats(root):
                 local_file_count += 1
                 total_file_size += os.path.getsize(file_path)
                 local_file_size += os.path.getsize(file_path)
+            if local_file_count <= 2 and local_file_size / 1024**2 <= 64 and os.path.abspath(os.path.join(dirname,os.path.pardir)) == os.path.abspath(root):
+                potential_zip_count += 1
         # print(local_file_size)
         # print(total_file_size)
         if local_file_count > 0:
@@ -41,12 +45,12 @@ def gather_stats(root):
 
     suggestion = "collation not advised."
     if folder_count > 0:
-        if folders_with_mean_filesize_lt_64mb/folder_count*100 > 10 and folders_with_lt_2_files/folder_count*100 > 10:
-            suggestion = "collation advised - high proportion of small files and small per-folder file counts."
-        elif folders_with_mean_filesize_lt_64mb/folder_count*100 > 15:
+        if folders_with_mean_filesize_lt_64mb/folder_count*100 > 10 and potential_zip_count/folder_count*100 > 10:
+            suggestion = "collation advised - high proportion of small files and collatable folders."
+        elif folders_with_mean_filesize_lt_64mb/folder_count*100 > 5:
             suggestion = "collation advised - high proportion of small files, although proportion of small per-folder file counts low."
-        elif folders_with_lt_2_files/folder_count*100 > 15:
-            suggestion = "collation advised - high proportion of small per-folder file counts, although proportion of small files low."
+        elif potential_zip_count/folder_count*100 > 5:
+            suggestion = "collation advised - high proportion of folders can be collated."
 
     if folder_count > 0:
         perc_small_files = folders_with_mean_filesize_lt_64mb/folder_count*100
@@ -66,6 +70,8 @@ def gather_stats(root):
     print(f"Number of folders with <= 2 files: {folders_with_lt_2_files}")
     print(f"Percentage of folders with mean file size <= 64 MiB: {perc_small_files:.2f} %")
     print(f"Percentage of folders with <= 2 files: {perc_low_file_count:.2f} %")
+    print(f"Potential number of zipped files: {potential_zip_count}")
+    print(f"Percentage of folders that could be zipped: {potential_zip_count/folder_count*100:.2f} %")
     print(f"Collation of files during backup is advised if both percentages are high.\n\u2794  Here: {suggestion}")
 
 print(f"Gathering stats for folder: {sys.argv[1]}...")
