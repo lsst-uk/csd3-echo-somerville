54a8a00997ca891fb29e263781403360e5485460
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-07-01 15:07:29 +0100

    added script o compare csv file lists

---
+++ scripts/compare_csv_file_lists.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="Compare CSV file lists in a log folder.")
+parser.add_argument("--path", '-p', help="Path to the log folder", required=True)
+args = parser.parse_args()
+
+log_folder = args.path
+
+def compare_csv_file_lists(log_folder):
+    """
+    Compare the CSV file lists in the specified log folder.
+
+    Args:
+        log_folder (str): The path to the log folder.
+
+    Returns:
+        None
+    """
+    csv_files = []
+    for file in os.listdir(log_folder):
+        if file.startswith("lsst-backup-logs-") and file.endswith(".csv") and file.__contains__('{{ ds_nodash }}'):
+            csv_files.append(file)
+    for csv_file in csv_files:
+        csv_file.replace("lsst-backup-logs-" + '{{ ds_nodash }}', "")
+        csv_file.replace(".csv", "")
+    csv_files = csv_files.sort()[-2:]
+
+    print(csv_files)
+
+if __name__ == "__main__":
+    compare_csv_file_lists(log_folder)
