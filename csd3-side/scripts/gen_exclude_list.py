6e068101c98f5f35ee04db1a685aff7f1eaf15ea
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-07-24 10:07:29 +0100

    building exclude list generator

---
+++ csd3-side/scripts/gen_exclude_list.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import os
+import sys
+
+def get_local_file_list(local_folder):
+    local_files = []
+    local_folders = []
+    
+    for root, dirs, files in os.walk(local_folder):
+        these_local_files = []
+        these_local_folders = []
+        for file in files:
+            these_local_files.append(os.path.join(root, file))
+        for folder in dirs:
+            these_local_folders.append(os.path.join(root, folder))
+        local_files.extend(these_local_files)
+        local_folders.extend(these_local_folders)
+    return local_files, local_folders
+
+def generate_exclude_list(csv_file):
+    exclude_list = []
+    folder_files = {}
+
+    # Read the CSV file using pandas
+    df = pd.read_csv(csv_file)
+    for _, row in df.iterrows():
+        local_folder = row['LOCAL_FOLDER']
+        local_path = row['LOCAL_PATH']
+        file_name = os.path.basename(local_path)
+
+        # Add the file to the folder_files dictionary
+        if local_folder not in folder_files:
+            folder_files[local_folder] = set()
+        folder_files[local_folder].add(file_name)
+
+    # Check if all files in each folder are in the CSV file
+    for folder, files in folder_files.items():
+        all_files_in_csv = all(file in df.columns for file in files)
+        if all_files_in_csv:
+            exclude_list.append(folder)
+
+    return exclude_list
+
+# Usage example
+csv_file = sys.argv[1]
+#LOCAL_FOLDER,LOCAL_PATH,FILE_SIZE,BUCKET_NAME,DESTINATION_KEY,CHECKSUM,ZIP_CONTENTS
+df = pd.read_csv(csv_file)
+local_folder = df['LOCAL_FOLDER'].iloc[0]
+df = df.drop(['LOCAL_FOLDER','FILE_SIZE','BUCKET_NAME','DESTINATION_KEY','CHECKSUM'], axis=1)
+
+local_files, local_folders = get_local_file_list(local_folder)
+
+print('Local folder:', local_folder)
+print('Local files:', local_files)
+print('Local folders:', local_folders)
+# exclude_list = generate_exclude_list(csv_file)
+# print(exclude_list)
