7730f4dc9ccece62191adb1c02ae6f48e0585fd4
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-07-25 12:08:56 +0100

    done

--- csd3-side/scripts/gen_exclude_list.py
+++ csd3-side/scripts/gen_exclude_list.py
@@ -4,7 +4,7 @@ import sys
 import dask.dataframe as dd
 
 csv_file = sys.argv[1]
-#LOCAL_FOLDER,LOCAL_PATH,FILE_SIZE,BUCKET_NAME,DESTINATION_KEY,CHECKSUM,ZIP_CONTENTS
+
 dtypes = {'LOCAL_FOLDER': 'str', 'LOCAL_PATH': 'str', 'FILE_SIZE': 'float', 'BUCKET_NAME': 'str', 'DESTINATION_KEY': 'str', 'CHECKSUM': 'str', 'ZIP_CONTENTS': 'str'}
 df = dd.read_csv(csv_file, dtype=dtypes).drop(['FILE_SIZE','BUCKET_NAME','DESTINATION_KEY','CHECKSUM'], axis=1).compute()
 local_fns = []
@@ -26,7 +26,7 @@ print(df.head())
 print(df.tail())
 df = df.groupby('LOCAL_FOLDER').agg(','.join).reset_index().dropna()
 print(df['LOCAL_FOLDER'].head())
-df.to_csv('test.csv', index=False)
+# df.to_csv('test.csv', index=False)
 
 
 j=0
@@ -47,8 +47,8 @@ for folder_row in df.iterrows():
         collated = False
 
     print(f"local_folder: {local_folder}")
-    print(f"logged_files: {logged_files}")
-    print(f"logged_zipped_files: {logged_zipped_files}")
+    # print(f"logged_files: {logged_files}")
+    # print(f"logged_zipped_files: {logged_zipped_files}")
     local_folders = []
     if collated:
         # extend_path = ''
@@ -58,7 +58,7 @@ for folder_row in df.iterrows():
                 lzf_list = lzf.split('/')
                 logged_zipped_files[i] = lzf_list[-1]
                 subfolders.extend(lzf_list[:-1])
-        print(f"subfolders: {subfolders}")
+        # print(f"subfolders: {subfolders}")
         print(f"number of unique subfolders: {len(set(subfolders))}")
         unique_subfolders = set(subfolders)
         if len(unique_subfolders) == 1:
@@ -67,24 +67,20 @@ for folder_row in df.iterrows():
         elif len(unique_subfolders) > 1:
             local_folders = [local_folder + '/' + subfolder for subfolder in unique_subfolders]
 
-          
-                # make a set from the first element of the split list and check length
-                    # if length is 1, then extend_path = '/' + lzf_list[0]
-                    # if length is > 1, then change local_folder to list of local_folders
         if local_folders == []:
             print(f'Local Folder: {local_folder}')
         else:
             print(f'Local Folders: {local_folders}')
         print('Collated')
         print('Verifying files...')
-        for root, dirs, files in os.walk(local_folder):
-            print('root',root)
-            print('dirs',dirs)
-            print('files',files)
+        # for root, dirs, files in os.walk(local_folder):
+            # print('root',root)
+            # print('dirs',dirs)
+            # print('files',files)
         if local_folders == []:
             files = [f for _,_,files in os.walk(local_folder) for f in files]
-            print('files',files)
-            print('logged_zipped_files',logged_zipped_files)
+            # print('files',files)
+            # print('logged_zipped_files',logged_zipped_files)
             if all([f in files for f in logged_zipped_files]) and len(files) == len(logged_zipped_files):
                 print('Verified')
                 exclude_list.append(local_folder)
@@ -96,18 +92,20 @@ for folder_row in df.iterrows():
             for lf in local_folders:
                 print(f'Verifying {lf}')
                 files = [f for _,_,files in os.walk(lf) for f in files]
-                print('files',files)
-                print('logged_zipped_files',logged_zipped_files)
+                # print('files',files)
+                # print('logged_zipped_files',logged_zipped_files)
                 total_files_len += len(files)
                 if all([f in logged_zipped_files for f in files]): # order important here as not expecting identical lists, just logged_zipped_files should contain all elements of files
                     verified_content += 1
-            print(f'verified_content: {verified_content}')
-            print(f'total_files_len: {total_files_len}')
-            print(f'len(logged_zipped_files): {len(logged_zipped_files)}')
-            print(f'len(local_folders): {len(local_folders)}')
+            # print(f'verified_content: {verified_content}')
+            # print(f'total_files_len: {total_files_len}')
+            # print(f'len(logged_zipped_files): {len(logged_zipped_files)}')
+            # print(f'len(local_folders): {len(local_folders)}')
             if verified_content == len(local_folders) and total_files_len == len(logged_zipped_files):
                 print('Verified')
                 exclude_list.append(lf)
+            else:
+                print('Unverified')
                
         
         
@@ -124,10 +122,6 @@ for folder_row in df.iterrows():
         else:
             print('Unverified')
 
-
-    j+=1
-    if j>5:
-        break
 print(exclude_list)
 with open('exclude_list.txt', 'w') as excl_f:
     excl_f.write(str(exclude_list))
