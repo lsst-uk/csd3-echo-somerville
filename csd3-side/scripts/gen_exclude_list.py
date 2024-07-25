8fa5eb02416603c6d4c2210727b05255fa6fd099
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-07-25 12:59:32 +0100

    Gen exclude list (#19)
    * need to generate exclude list to use in speeding up lsst-backup.py
    
    * removing local_files from csv
    
    * .
    
    * .
    
    * changed to just check if a local file is listed in the upload log
    
    * .
    
    * .
    
    * debug
    
    * debug
    
    * .
    
    * .
    
    * info
    
    * .
    
    * info
    
    * feedback
    
    * .
    
    * flush
    
    * done?
    
    * write exclude list
    
    * zips
    
    * .
    
    * dtype
    
    * loc
    
    * str
    
    * sum
    
    * .
    
    * zips
    
    * zip_contents
    
    * .
    
    * verify zipped_dirs
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * debug
    
    * .
    
    * unique issue?
    
    * local_folder variable?
    
    * .
    
    * need all local folders - useful!
    
    * .
    
    * .
    
    * .
    
    * .
    
    * syntax
    
    * include /
    
    * .
    
    * tail
    
    * using LOCAL_FILENAME in place of LOCAL_PATH with BASEPATH stripped
    
    * .
    
    * groupby
    
    * groupby().sum()
    
    * .
    
    * transform
    
    * .
    
    * .
    
    * .
    
    * agg
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * testing agg
    
    * fix isna to ''
    
    * iter folder_rows
    
    * .
    
    * .
    
    * .
    
    * verify files and collated files - first try
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * extend_path
    
    * done
    
    * redo walk
    
    * .
    
    * .
    
    * subfolders
    
    * .
    
    * is to ==
    
    * debug
    
    * verify lens and content separately for lists
    
    * debug
    
    * list comp ordering
    
    * done
    
    * done

--- csd3-side/scripts/gen_exclude_list.py
+++ csd3-side/scripts/gen_exclude_list.py
@@ -1,57 +1,131 @@
+# Script to generate an exclude list for use with lsst-backup.py
+# Use with caution, this script verifies the existence of files reported in an upload in the local filesystem - it does not verify their existence on the remote server.
+
 import pandas as pd
 import os
 import sys
 import dask.dataframe as dd
 
-def remove_local_files(df,local_folder):
-    for root, dirs, files in os.walk(local_folder):
-        print(len(df))
-        these_local_files = []
-        for f in files:
-            these_local_files.append(os.path.join(root, f))
-        print('these_local_files:', these_local_files)
-        df = df[~df['LOCAL_PATH'].isin(these_local_files)]
-
-def generate_exclude_list(df, local_folder):
-    exclude_list = []
-    folder_files = {}
-
-    # Read the CSV file using dask
-    for _, row in df.iterrows():
-        file_name = os.path.basename(local_folder)
-
-        # Add the file to the folder_files dictionary
-        if local_folder not in folder_files:
-            folder_files[local_folder] = set()
-        folder_files[local_folder].add(file_name)
-
-    # Check if all files in each folder are in the CSV file
-    for folder, files in folder_files.items():
-        all_files_in_csv = all(file in df.columns for file in files)
-        if all_files_in_csv:
-            exclude_list.append(folder)
-
-    return exclude_list
-
-# Usage example
 csv_file = sys.argv[1]
-#LOCAL_FOLDER,LOCAL_PATH,FILE_SIZE,BUCKET_NAME,DESTINATION_KEY,CHECKSUM,ZIP_CONTENTS
-df = dd.read_csv(csv_file).drop(['FILE_SIZE','BUCKET_NAME','DESTINATION_KEY','CHECKSUM'], axis=1)
-local_folder_series = df['LOCAL_FOLDER'].compute()
-local_folder = local_folder_series.iloc[0]
-print(local_folder)
-del local_folder_series
-df = df.drop(['LOCAL_FOLDER'], axis=1).compute()
+
+dtypes = {'LOCAL_FOLDER': 'str', 'LOCAL_PATH': 'str', 'FILE_SIZE': 'float', 'BUCKET_NAME': 'str', 'DESTINATION_KEY': 'str', 'CHECKSUM': 'str', 'ZIP_CONTENTS': 'str'}
+df = dd.read_csv(csv_file, dtype=dtypes).drop(['FILE_SIZE','BUCKET_NAME','DESTINATION_KEY','CHECKSUM'], axis=1).compute()
+local_fns = []
+for row in df.iterrows():
+    print(row[1]['LOCAL_PATH'])
+    print(row[1]['LOCAL_FOLDER'])
+    
+    if row[1]['LOCAL_PATH'].startswith(row[1]['LOCAL_FOLDER']):
+        local_fns.append(row[1]['LOCAL_PATH'][len(row[1]['LOCAL_FOLDER'])+1:])
+    else:
+        local_fns.append(row[1]['LOCAL_PATH'])
+df['LOCAL_FILENAME'] = local_fns
+df = df.drop(['LOCAL_PATH'], axis=1)
+df.loc[df['ZIP_CONTENTS'].isna(), 'ZIP_CONTENTS'] = ''
+
+
 
 print(df.head())
-print(len(df))
+print(df.tail())
+df = df.groupby('LOCAL_FOLDER').agg(','.join).reset_index().dropna()
+print(df['LOCAL_FOLDER'].head())
+# df.to_csv('test.csv', index=False)
 
-remove_local_files(df,local_folder)
 
-print(len(df))
+j=0
+exclude_list = []
+for folder_row in df.iterrows():
+    logged_files = folder_row[1]['LOCAL_FILENAME'].split(',')
+    logged_zipped_files = folder_row[1]['ZIP_CONTENTS'].split(',')
+    # logged_zipped_files = [szf.split('/')[-1] for szf in logged_zipped_files]
+    local_folder = folder_row[1]['LOCAL_FOLDER']
+    collated = False
+    if logged_files[0].endswith('.zip') and logged_zipped_files[0] != '':
+        collated = True
+    elif logged_files[0].endswith('.zip') and logged_zipped_files[0] == '':
+        raise ValueError('Zip file with no contents')
+    elif logged_files[0].endswith('.zip') == False and logged_zipped_files[0] != '':
+        raise ValueError('Non-zip file with zip contents')
+    elif logged_files[0].endswith('.zip') == False and logged_zipped_files[0] == '':
+        collated = False
 
-# print('Local folder:', local_folder)
-# print('Local files:', local_files)
-# print('Local folders:', local_folders)
-# exclude_list = generate_exclude_list(csv_file)
-# print(exclude_list)
+    print(f"local_folder: {local_folder}")
+    # print(f"logged_files: {logged_files}")
+    # print(f"logged_zipped_files: {logged_zipped_files}")
+    local_folders = []
+    if collated:
+        # extend_path = ''
+        subfolders = []
+        for i, lzf in enumerate(logged_zipped_files):
+            if '/' in lzf:
+                lzf_list = lzf.split('/')
+                logged_zipped_files[i] = lzf_list[-1]
+                subfolders.extend(lzf_list[:-1])
+        # print(f"subfolders: {subfolders}")
+        print(f"number of unique subfolders: {len(set(subfolders))}")
+        unique_subfolders = set(subfolders)
+        if len(unique_subfolders) == 1:
+            extend_path = '/' + subfolders[0]
+            local_folder += extend_path
+        elif len(unique_subfolders) > 1:
+            local_folders = [local_folder + '/' + subfolder for subfolder in unique_subfolders]
+
+        if local_folders == []:
+            print(f'Local Folder: {local_folder}')
+        else:
+            print(f'Local Folders: {local_folders}')
+        print('Collated')
+        print('Verifying files...')
+        # for root, dirs, files in os.walk(local_folder):
+            # print('root',root)
+            # print('dirs',dirs)
+            # print('files',files)
+        if local_folders == []:
+            files = [f for _,_,files in os.walk(local_folder) for f in files]
+            # print('files',files)
+            # print('logged_zipped_files',logged_zipped_files)
+            if all([f in files for f in logged_zipped_files]) and len(files) == len(logged_zipped_files):
+                print('Verified')
+                exclude_list.append(local_folder)
+            else:
+                print('Unverified')
+        else:
+            total_files_len = 0
+            verified_content = 0
+            for lf in local_folders:
+                print(f'Verifying {lf}')
+                files = [f for _,_,files in os.walk(lf) for f in files]
+                # print('files',files)
+                # print('logged_zipped_files',logged_zipped_files)
+                total_files_len += len(files)
+                if all([f in logged_zipped_files for f in files]): # order important here as not expecting identical lists, just logged_zipped_files should contain all elements of files
+                    verified_content += 1
+            # print(f'verified_content: {verified_content}')
+            # print(f'total_files_len: {total_files_len}')
+            # print(f'len(logged_zipped_files): {len(logged_zipped_files)}')
+            # print(f'len(local_folders): {len(local_folders)}')
+            if verified_content == len(local_folders) and total_files_len == len(logged_zipped_files):
+                print('Verified')
+                exclude_list.append(lf)
+            else:
+                print('Unverified')
+               
+        
+        
+    else:
+        print(f'Folder: {local_folder}')
+        print('Uncollated')
+        print('Verifying files...')
+        files = [f for _,_,files in os.walk(local_folder) for f in files]
+        print(files)
+        print(logged_files)
+        if all([f in files for f in logged_files]) and len(files) == len(logged_files):
+            print('Verified')
+            exclude_list.append(local_folder)
+        else:
+            print('Unverified')
+
+print(exclude_list)
+with open('exclude_list.txt', 'w') as excl_f:
+    excl_f.write(str(exclude_list))
+    excl_f.write('\n')
