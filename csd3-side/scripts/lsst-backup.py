55484caa22e510b8b5aa4679ca015c66c7a5eacc
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-05 13:47:23 +0100

    freeing up memory not working and possibly redundant.

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -422,9 +422,9 @@ def upload_and_callback(s3_host, access_key, secret_key, bucket_name, folder, fi
         logfile.write(f'{result}\n')
     return None
 
-def free_up_zip_memory(to_collate, parent_folder, index):
-    del to_collate[parent_folder]['zips'][index]['zip_contents']
-    print(f'Deleted zip contents object for {parent_folder}, zip index {index}, to free memory.')
+# def free_up_zip_memory(to_collate, parent_folder, index):
+#     del to_collate[parent_folder]['zips'][index]['zip_contents']
+#     print(f'Deleted zip contents object for {parent_folder}, zip index {index}, to free memory.')
 
 def process_files(s3_host, access_key, secret_key, bucket_name, current_objects, exclude, local_dir, destination_dir, nprocs, perform_checksum, dryrun, log, global_collate, use_compression):
     """
@@ -686,22 +686,22 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
                             # #[os.sep.join([destination_dir, os.path.relpath(filename, local_dir)]) for filename in folder_files]
                             # to_collate[parent_folder][id]['zip_object_name'] = 
 
-                            tc_index = len(to_collate[parent_folder]['zips']) - 1
+                            # tc_index = len(to_collate[parent_folder]['zips']) - 1
 
                             # check if zip_object_name exists in bucket and get its checksum
-                            if current_objects.isin([to_collate[parent_folder]['zips'][tc_index]['zip_object_name']]).any():
-                                existing_zip_checksum = bm.get_resource(access_key, secret_key, s3_host).Object(bucket_name,to_collate[parent_folder]['zips'][tc_index]['zip_object_name']).e_tag.strip('"')
+                            if current_objects.isin([to_collate[parent_folder]['zips'][-1]['zip_object_name']]).any():
+                                existing_zip_checksum = bm.get_resource(access_key, secret_key, s3_host).Object(bucket_name,to_collate[parent_folder]['zips'][-1]['zip_object_name']).e_tag.strip('"')
                                 checksum_hash = hashlib.md5(zip_data)
                                 checksum_string = checksum_hash.hexdigest()
 
                                 if checksum_string == existing_zip_checksum:
-                                    print(f'Zip file {to_collate[parent_folder]["zips"][tc_index]["zip_object_name"]} already exists and checksums match - skipping.')
+                                    print(f'Zip file {to_collate[parent_folder]["zips"][-1]["zip_object_name"]} already exists and checksums match - skipping.')
                                     continue
                                 
                             # upload zipped folders
                             total_size_uploaded += len(zip_data)
                             total_files_uploaded += 1
-                            print(f"Uploading {to_collate[parent_folder]['zips'][tc_index]['zip_object_name']}.")
+                            print(f"Uploading {to_collate[parent_folder]['zips'][-1]['zip_object_name']}.")
                             results.append(collate_ul_pool.apply_async(upload_and_callback, args=(
                                 s3_host,
                                 access_key,
@@ -709,8 +709,8 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
                                 bucket_name,
                                 parent_folder,
                                 zip_data,
-                                to_collate[parent_folder]['zips'][tc_index]['zip_contents'],
-                                to_collate[parent_folder]['zips'][tc_index]['zip_object_name'],
+                                to_collate[parent_folder]['zips'][-1]['zip_contents'],
+                                to_collate[parent_folder]['zips'][-1]['zip_object_name'],
                                 perform_checksum,
                                 dryrun,
                                 processing_start,
@@ -719,7 +719,7 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
                                 total_size_uploaded,
                                 total_files_uploaded,
                                 True),
-                                callback=lambda _: free_up_zip_memory(to_collate, parent_folder, tc_index),
+                                # callback=lambda _: free_up_zip_memory(to_collate, parent_folder, tc_index),
                             ))
 
     pool.close()
