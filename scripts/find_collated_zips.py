fc951626f1ea7a284c82e6f943a885bc7f4bc748
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-07 16:51:50 +0100

    .

--- scripts/find_collated_zips.py
+++ scripts/find_collated_zips.py
@@ -78,7 +78,7 @@ def verify_zip_contents(zipfile_df, all_keys_s, debug):
 def append_contents_to_zipfile_path(zipfile_df):
     print(zipfile_df.iloc[0]['zipfile'])
     print(zipfile_df.iloc[0]['contents'])
-    path_stub = '/'.join(zipfile_df.iloc[0]['zipfile'].split('/')[-1])
+    path_stub = '/'.join(zipfile_df.iloc[0]['zipfile'].split('/')[:-1])
     print(path_stub)
     # zipfile_df['zipfile'] = zipfile_df['zipfile'].apply(lambda x: f'/mnt/data/{x}')
     # return zipfile_df
