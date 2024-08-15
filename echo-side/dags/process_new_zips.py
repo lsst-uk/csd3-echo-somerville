4e401a529aa24f2c025abab6df09b5ccc1e35385
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-15 10:09:42 +0100

    .

--- echo-side/dags/process_new_zips.py
+++ echo-side/dags/process_new_zips.py
@@ -4,17 +4,19 @@ from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from datetime import timedelta, datetime
 
+bucket_names = []
+
 def dl_bucket_names(**kwargs):
     import json
     import requests
-    bucket_names = []
+    global bucket_names
     url = kwargs['url']
     r = requests.get(url)
     buckets = json.loads(r.text)
     for bucket in buckets:
         bucket_names.append(bucket['name'])
-    kwargs['ti'].xcom_push(key='bucket_names', value=bucket_names)
-    return bucket_names
+    # kwargs['ti'].xcom_push(key='bucket_names', value=bucket_names)
+    # return bucket_names
     
 # Define default arguments for the DAG
 default_args = {
@@ -39,22 +41,21 @@ with dag:
         task_id = 'get_bucket_names',
         python_callable = dl_bucket_names,
         op_kwargs={'url':'https://raw.githubusercontent.com/lsst-uk/csd3-echo-somerville/main/echo-side/bucket_names/bucket_names.json'},
-        show_return_value_in_logs=True,
     )
 
-    # if len() > 0:
-    #     for i, bucket_name in enumerate(bucket_names):
-    #         process_zips_task = KubernetesPodOperator(
-    #             task_id=f'process_zips_{i}',
-    #             image='ghcr.io/lsst-uk/csd3-echo-somerville:latest',
-    #             cmds=['./entrypoint.sh'],
-    #             arguments=['python', 'csd3-echo-somerville/scripts/process_collated_zips.py', '--bucket_name', bucket_name, '--extract', '--nprocs', '16'],
-    #             env_vars={
-    #                 'ECHO_S3_ACCESS_KEY': Variable.get("ECHO_S3_ACCESS_KEY"),
-    #                 'ECHO_S3_SECRET_KEY': Variable.get("ECHO_S3_SECRET_KEY"),
-    #             },
-    #             get_logs=True,
-    #         )
-    # else:
-    #     print('No bucket names found.')
+    if len(bucket_names) > 0:
+        for i, bucket_name in enumerate(bucket_names):
+            process_zips_task = KubernetesPodOperator(
+                task_id=f'process_zips_{i}',
+                image='ghcr.io/lsst-uk/csd3-echo-somerville:latest',
+                cmds=['./entrypoint.sh'],
+                arguments=['python', 'csd3-echo-somerville/scripts/process_collated_zips.py', '--bucket_name', bucket_name, '--extract', '--nprocs', '16'],
+                env_vars={
+                    'ECHO_S3_ACCESS_KEY': Variable.get("ECHO_S3_ACCESS_KEY"),
+                    'ECHO_S3_SECRET_KEY': Variable.get("ECHO_S3_SECRET_KEY"),
+                },
+                get_logs=True,
+            )
+    else:
+        print('No bucket names found.')
             
