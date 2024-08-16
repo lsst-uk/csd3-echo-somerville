3e7e00586fbd6e6c4e325892d722f2d468d04fe7
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-16 16:37:18 +0100

    split into two dags to not hurt brain

--- echo-side/dags/monitor.py
+++ echo-side/dags/monitor.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from datetime import timedelta, datetime
 from kubernetes.client import models as k8s
-from airflow.models.baseoperator import chain_linear
+from airflow.models.baseoperator import chain
 
 # Create k8s storage mount 
 
@@ -37,14 +37,6 @@ def dl_bucket_names(url):
     return bucket_names
 
 bucket_names = dl_bucket_names('https://raw.githubusercontent.com/lsst-uk/csd3-echo-somerville/main/echo-side/bucket_names/bucket_names.json')
-
-def get_new_csvs(bucket_name):
-    new_csvs[bucket_name] = []
-    with open(''.join([f'/lsst-backup-logs/new-csv-files-{bucket_name}','{{ ds_nodash }}','.txt']), 'r') as f:
-        for line in f:
-            new_csvs[bucket_name].append(line.strip())
-    if len(new_csvs[bucket_name]) == 0:
-        del new_csvs[bucket_name]
 
 # Instantiate the DAG
 with DAG(
@@ -90,39 +82,13 @@ with DAG(
         get_logs=True,
     ) for bucket_name in bucket_names]
 
-    get_new_csvs_task = [ PythonOperator(
-        task_id=f'get_new_csvs-{bucket_name}',
-        python_callable=get_new_csvs,
-        op_args=[bucket_name],
-    ) for bucket_name in bucket_names ]
 
-    check_uploads = []
-
-    for bucket_new_csvs in new_csvs.items():
-        bucket_name = bucket_new_csvs[0]
-        new_csvs_list = bucket_new_csvs[1]
-        for csv in new_csvs_list:
-            check_uploads.append(
-                KubernetesPodOperator(
-                task_id=f'check_{csv}',
-                image='ghcr.io/lsst-uk/csd3-echo-somerville:latest',
-                cmds=['./entrypoint.sh'],
-                arguments=['python', 'csd3-echo-somerville/scripts/check_upload.py', bucket_name, csv],
-                env_vars={
-                    'ECHO_S3_ACCESS_KEY': Variable.get("ECHO_S3_ACCESS_KEY"),
-                    'ECHO_S3_SECRET_KEY': Variable.get("ECHO_S3_SECRET_KEY"),
-                },
-                volumes=[logs_volume],
-                volume_mounts=[logs_volume_mount],
-                get_logs=True,
-                )
-            )
+    ### TODO
+    ## Add a DAG trigger to run check_uploads_dag.py
 
     # Set the task sequence
-    chain_linear(
+    chain(
         list_csv_files,
         compare_csv_file_lists,
-        get_new_csvs_task,
-        check_uploads
     )
             
