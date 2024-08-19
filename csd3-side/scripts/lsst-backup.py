0d3199a75243cd244be2d1d83d3518a1cae95094
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-19 11:39:36 +0100

    print mem_per_core limit

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -842,6 +842,8 @@ if __name__ == '__main__':
     else:
         mem_per_core = mem_per_core * 1024**2 # convert to bytes
     
+    print(f'max_mem_per_core: {max_mem_per_core/1024**2} MiB')
+    
     if args.exclude:
         exclude = pd.Series(args.exclude)
     else:
