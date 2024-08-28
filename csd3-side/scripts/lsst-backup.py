84acebcb1a8b1c0842cc9da66d94c3adef3efede
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-28 10:56:56 +0100

    less verbose

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -782,7 +782,7 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
         zul_results = []
         total_zips = len(zip_results)
         while zipped < total_zips:
-            print(f'Zipped {zipped} of {total_zips} zip files.', flush=True)
+            # print(f'Zipped {zipped} of {total_zips} zip files.', flush=True)
             for i, result in enumerate(zip_results):
                 if result is not None:
                     if result.ready():
