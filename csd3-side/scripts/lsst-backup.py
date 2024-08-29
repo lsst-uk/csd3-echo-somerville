f1f1c97b5bd45e503dac0849443118ba928f78d3
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-08-29 09:57:06 +0100

    .

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -880,8 +880,9 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
             for i, result in enumerate(results):
                 if not result.ready():
                     print(f'{uploads[i]}')
+                    print(waited_time)
                     if waited_time > 10: # short timeout for testing
-                        result.wait(timeout=0)
+                        result.wait(timeout=1)
                 else:
                     uploads[i]['uploaded'] = True
                     uploads[i]['folder_files'] = None # free up memory
