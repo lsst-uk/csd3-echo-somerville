416fdcba7a807f1ba70b1d9f36eba7efc5b4d71b
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-09-05 09:19:27 +0100

    .

--- csd3-side/scripts/lsst-backup-dask.py
+++ csd3-side/scripts/lsst-backup-dask.py
@@ -1161,10 +1161,9 @@ if __name__ == '__main__':
     ############################
     #        Dask Setup        #
     ############################
-    client_memory = virtual_memory().total//nprocs
     mem_per_worker = virtual_memory().total//nprocs
-    print(f'Threads: {threads}, Client memory: {client_memory}, memory per worker: {mem_per_worker}')
-    client = Client(n_workers=nprocs//threads,threads_per_worker=threads,memory_limit=client_memory) #,silence_logs=ERROR
+    print(f'Threads per worker: {threads}, Memory per worker: {mem_per_worker}')
+    client = Client(n_workers=nprocs//threads,threads_per_worker=threads,memory_limit=mem_per_worker) #,silence_logs=ERROR
 
     print(f'Dask Client: {client}', flush=True)
 
