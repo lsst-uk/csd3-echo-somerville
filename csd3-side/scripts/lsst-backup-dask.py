d01a6ef6506cab80f1fae773b731d405d5584b5c
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-09-10 11:07:11 +0100

    .

--- csd3-side/scripts/lsst-backup-dask.py
+++ csd3-side/scripts/lsst-backup-dask.py
@@ -1220,8 +1220,6 @@ if __name__ == '__main__':
     else:
         current_objects['METADATA'] = None
 
-    print(current_objects['METADATA'].dropna(), flush=True)
-
     # current_objects.to_csv('current_objects.csv', index=False)
 
     ## check if log exists in the bucket, and download it and append top it if it does
