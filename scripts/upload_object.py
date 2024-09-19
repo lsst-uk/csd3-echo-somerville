47e05fe3d69acf93449c45955fc21fd594c7ae8e
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-09-19 12:39:10 +0100

    66 offload large files (#67)
    * added cli to upload_object
    
    * .
    
    * using subprocess
    
    * try list remove
    
    * permissionerror handling
    
    * done

--- scripts/upload_object.py
+++ scripts/upload_object.py
@@ -3,13 +3,27 @@
 # D. McKay Feb 2024
 
 """
-This script allows you to upload a single file given bucket name, URI and local path.
+This script allows you to upload a single file given bucket name, object_name and local path.
 """
 
 import sys
 import warnings
+import argparse
 
 import bucket_manager.bucket_manager as bm
+
+
+
+parser = argparse.ArgumentParser(description='Upload a file to an S3 bucket.')
+parser.add_argument('--bucket-name', '-b', type=str, help='The name of the S3 bucket.')
+parser.add_argument('--object-name', '-o', type=str, help='The name of the object to upload.')
+parser.add_argument('--local-path', '-p', type=str, help='The local path to the file to upload.')
+
+args = parser.parse_args()
+
+bucket_name = args.bucket_name
+object_name = args.object_name
+local_path = args.local_path
 
 s3_host = 'echo.stfc.ac.uk'
 try:
@@ -24,18 +38,17 @@ warnings.filterwarnings('ignore')
 
 s3 = bm.get_resource(access_key, secret_key, s3_host)
 
-bucket_name = sys.argv[1]
-URI = sys.argv[2]
-local_path = sys.argv[3]
-
 if not bucket_name in bm.bucket_list(s3):
     sys.exit('Bucket does not exist.')
 
 bucket = s3.Bucket(bucket_name)
 
-# if not URI in bm.object_list(bucket):
+# if not object_name in bm.object_list(bucket):
 #     sys.exit('Object does not exist.')
 
 
-response = bucket.upload_file(local_path, URI) 
-print(response)
+response = bucket.upload_file(local_path, object_name) 
+if response:
+    print(response)
+else:
+    print('Upload successful.')
