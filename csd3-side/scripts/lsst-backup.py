5153a14aa9a47da7b4b90ba88aad7c77cd836cc3
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-10-15 12:47:59 +0100

    direct to pandas

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -701,7 +701,7 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
         exit(1)
 
     #recursive loop over local folder
-    to_collate = {'id':[],'object_names':[],'file_paths':[],'size':[]} # to be used for storing file lists to be collated
+    # to_collate = {'id':[],'object_names':[],'file_paths':[],'size':[]} # to be used for storing file lists to be collated
     total_all_folders = 0
     total_all_files = 0
     folder_num = 0
@@ -982,12 +982,12 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
                             print(f'Zip file {destination_dir}/collated_{i+1}.zip already exists but file lists do not match - reuploading.')
 
             # Create dict for zip files
-            for i, file_paths in enumerate(zip_batch_files):
-                to_collate['id'].append(i)
-                to_collate['object_names'].append(zip_batch_object_names[i])
-                to_collate['file_paths'].append(file_paths)
-                to_collate['size'].append(zip_batch_sizes[i])
-            to_collate = pd.DataFrame.from_dict(to_collate, columns=['id', 'object_names', 'file_paths', 'size'], index='id')
+            # for i, file_paths in enumerate(zip_batch_files):
+            #     to_collate['id'].append(i)
+            #     to_collate['object_names'].append(zip_batch_object_names[i])
+            #     to_collate['file_paths'].append(file_paths)
+            #     to_collate['size'].append(zip_batch_sizes[i])
+            to_collate = pd.DataFrame([[i for i in range(len(zip_batch_files))],zip_batch_object_names, zip_batch_files, zip_batch_sizes], columns=['id', 'object_names', 'file_paths', 'size'], index='id')
             client.scatter(to_collate) 
             del zip_batch_files, zip_batch_object_names, zip_batch_sizes
         else:
