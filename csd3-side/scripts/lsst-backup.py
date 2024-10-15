8b6186a682299ea32193ed6f63a79c6c12662eb4
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-10-15 16:11:31 +0100

    .

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -996,6 +996,7 @@ def process_files(s3_host, access_key, secret_key, bucket_name, current_objects,
             # print(f'id: {[i for i in range(len(zip_batch_files))]}')
 
             to_collate = pd.DataFrame.from_dict(to_collate_list)
+            print(to_collate)
             client.scatter(to_collate) 
             del zip_batch_files, zip_batch_object_names, zip_batch_sizes
         else:
