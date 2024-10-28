0a98c5ac80a35c9e7d569758eb053dce191a1163
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-10-28 16:00:50 +0000

    lightweight progress tracker

---
+++ csd3-side/scripts/backup_progress.py
@@ -0,0 +1,23 @@
+import tqdm
+import sys
+import string
+
+logp = sys.argv[1]
+
+total = 0
+
+with open(logp, 'r') as logf:
+    for line in logf:
+        if 'Folders: ' in line and '; Files: ' in line:
+            total = float(line.split()[3].strip().translate(str.maketrans('', '', string.punctuation)))
+            break
+with tqdm.tqdm(total=total) as pbar:
+    progress = 0
+    while True:
+        with open(logp, 'r') as logf:
+            for line in logf:
+                if 'Folders: ' in line and '; Files: ' in line:
+                    prog = float(line.split()[1].strip().translate(str.maketrans('', '', string.punctuation)))
+                    if prog > progress:
+                        progress = prog
+                        pbar.update(progress)
