11693f6dd2f5c00b2fcb6d41436b65215d100ca5
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-11-05 11:53:04 +0000

    fix template path

--- csd3-side/scripts/backup_sf_list.sh
+++ csd3-side/scripts/backup_sf_list.sh
@@ -5,7 +5,7 @@ for sf in $(cat $sf_list_path);
 do
         mkdir $sf
         cd $sf
-        sed "s/SUBFOLDER/$sf/g" $template_path > $sf.yaml
+        sed "s/SUBFOLDER/$sf/g" ../$template_path > $sf.yaml
         date >> ${sf}_timings.log
         python ../../../scripts/lsst-backup.py --config-file $sf.yaml > $sf.log 2> $sf.err
         date >> ${sf}_timings.log
