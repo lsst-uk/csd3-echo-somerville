a4c3a84029f3674b76883ddc8aa7aa81219a44e2
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-11-13 13:58:31 +0000

    undo test change

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1463,9 +1463,7 @@ if __name__ == '__main__':
             print(f"Current objects (with matching prefix; excluding collated zips): {len(current_objects[current_objects['CURRENT_OBJECTS'].str.contains('collated_') == False])}", flush=True)
             print(f'Obtaining current object metadata, elapsed time = {datetime.now() - start}', flush=True)
             if api == 's3':
-                current_objects = dd.from_pandas(current_objects, npartitions=48)
                 current_objects['METADATA'] = current_objects['CURRENT_OBJECTS'].apply(find_metadata, bucket=bucket) # can't Daskify this without passing all bucket objects
-                current_objects = current_objects.compute()
             elif api == 'swift':
                 current_objects = dd.from_pandas(current_objects, npartitions=48)
                 current_objects['METADATA'] = current_objects['CURRENT_OBJECTS'].apply(find_metadata_swift, conn=s3, container_name=bucket_name)
