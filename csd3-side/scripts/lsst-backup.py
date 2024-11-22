3e837b0dde38556e259553bbe402e8d4b77c218d
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-11-22 13:57:51 +0000

    .

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -178,7 +178,8 @@ def zip_and_upload(df, s3, bucket_name, api, destination_dir, local_dir, total_s
     # print('in zip_and_upload', flush=True)
     print(df, flush=True)
     print(df['file_paths'], flush=True)
-    file_paths = list(literal_eval(df['file_paths'].iloc[0].values()[0]))
+    print(type(df['file_paths']), flush=True)
+    file_paths = list(literal_eval(str(df['file_paths'])))
     print(file_paths, flush=True)
     id = df['id'].iloc[0]
     #############
