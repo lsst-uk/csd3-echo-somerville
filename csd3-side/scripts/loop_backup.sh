e0499537b863da708e6ca4f2b5006b2043fd95c2
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-12-23 10:53:24 +0000

    .

--- csd3-side/scripts/loop_backup.sh
+++ csd3-side/scripts/loop_backup.sh
@@ -5,9 +5,13 @@
 config_file=$1
 collate_list_file=$2
 first_run=true
+date
+echo 'Looping backup...'
 while [ $(grep -c True $collate_list_file) -gt 0 ] || [ $first_run = true ];
 do
     echo 'while'
     python ../../scripts/lsst-backup.py --config-file $config_file
     first_run=false
 done
+date
+echo 'Looped backup completed.'
