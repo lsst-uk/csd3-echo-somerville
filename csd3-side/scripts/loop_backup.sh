3c54f034c0c5cb41cb0349f3ee36234b5e747352
Dave McKay <d.mckay@epcc.ed.ac.uk>
2024-12-23 11:17:06 +0000

    added cli

--- csd3-side/scripts/loop_backup.sh
+++ csd3-side/scripts/loop_backup.sh
@@ -2,20 +2,50 @@
 # Loop backup script
 # This script will run the backup script until all the files are backed up
 # It's kind of brute force.
-config_file=$1
+# D. McKay Dec 2024
 
+function display_help {
+    echo "Usage: $0 [options]"
+    echo
+    echo "Options:"
+    echo "  --config-file, -c FILE   Specify the configuration file."
+    echo "  -y                       Automatically continue without prompting."
+    echo "  -h, --help               Display this help message."
+    echo
+}
+
+# Check for help option
+for arg in "$@"; do
+    if [[ "$arg" == "--help" || "$arg" == "-h" ]]; then
+        display_help
+        exit 0
+    fi
+done
+
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --config-file|-c) config_file="$2"; shift ;;
+        -y) continue='y' ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ -z "$config_file" ]; then
+    echo "Error: --config-file or -c is required"
+    display_help
+    exit 1
+fi
 
 date
 echo 'Looping backup...'
 
 butler_prefix=$(grep 'S3_prefix:' $config_file | awk '{print $2}')
-echo 'Using Butler prefix: ' $butler_prefix
 local_path=$(grep 'local_path:' $config_file | awk '{print $2}')
-echo 'Using local path: ' $local_path
 local_path_from_butler_prefix=${local_path#*${butler_prefix}/}
-echo 'Local path from Butler prefix: ' $local_path_from_butler_prefix
 local_path_hyphens=$(echo $local_path_from_butler_prefix | sed 's/\//\-/g')
-echo 'Local path with hyphens: ' $local_path_hyphens
 collate_list_file=${butler_prefix}-${local_path_hyphens}-collate-list.csv
 
 echo 'Using collate list file: ' $collate_list_file
