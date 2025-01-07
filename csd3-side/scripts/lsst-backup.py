f5fdc113fe171b8b8445da5e706e528057eb7a5d
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-01-07 15:15:30 +0000

    convert to dask dataframe

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1204,6 +1204,7 @@ def process_files(s3, bucket_name, api, current_objects, exclude, local_dir, des
             if not current_objects.empty:
                 # now using pandas for both current_objects and to_collate - this could be re-written to using vectorised operations
                 client.scatter(current_objects)
+                to_collate = dd.from_pandas(to_collate, npartitions=len(client.scheduler_info()['workers'])*10)
                 to_collate['upload'] = to_collate.apply(
                     compare_zip_contents_bool, (current_objects, destination_dir), meta=pd.Series(), axis=1)
                 print('Comparison complete.', flush=True)
