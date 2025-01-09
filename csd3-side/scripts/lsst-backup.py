bac481f5261a212ca0f7c291a5e630b5ee69c798
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-01-09 12:37:17 +0000

    column name clash

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1208,9 +1208,12 @@ def process_files(s3, bucket_name, api, current_objects, exclude, local_dir, des
                 # client.scatter([current_objects,to_collate])
                 # to_collate = dd.from_pandas(to_collate, npartitions=len(client.scheduler_info()['workers'])*10)
                 print('Created Dask dataframe for to_collate.', flush=True)
-                to_collate['upload'] = to_collate.apply(compare_zip_contents_bool, args=(current_objects, destination_dir), meta={'upload':'bool'}, axis=1)
-                print('Comparison complete.', flush=True)
+                to_collate['_upload'] = to_collate.apply(compare_zip_contents_bool, args=(current_objects, destination_dir), meta={'upload':'bool'}, axis=1)
+                to_collate = to_collate.drop(columns=['upload'])
+                to_collate = to_collate.rename(columns={'_upload':'upload'})
+                print('Comparison setup.', flush=True)
                 to_collate = to_collate.compute()
+                print('Comparison complete.', flush=True)
                 exit()
 
         if save_collate_file:
