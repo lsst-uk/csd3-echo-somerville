635d82b8dc965f3b179c0e93e7b4c4337704a725
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-01-15 11:12:32 +0000

    don't print cmp

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -136,7 +136,7 @@ def compare_zip_contents_bool(collate_object_names, id: int, current_objects: pd
     # dprint(f'current_objects["METADATA"].values[362681][0]: {current_objects["METADATA"].values[362681][0]}', flush=True)
     # dprint(f'type current_objects["METADATA"].values[362681][0]: {type(current_objects["METADATA"].values[362681][0])}', flush=True)
     cmp = [ x.replace(destination_dir+'/', '') for x in collate_object_names]
-    dprint(f'cmp: {cmp}', flush=True)
+    # dprint(f'cmp: {cmp}', flush=True)
     dprint(f'len cmp: {len(cmp)}', flush=True)
     if not current_objects.empty:
         dprint(current_objects['METADATA'], flush=True)
