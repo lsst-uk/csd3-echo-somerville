9d6e489672be56176bd0de9aea223cb690fde540
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-02-03 15:41:02 +0000

    dprint

--- scripts/process_collated_zips.py
+++ scripts/process_collated_zips.py
@@ -233,7 +233,7 @@ def main():
     with Client(n_workers=n_workers,threads_per_worker=threads_per_worker,memory_limit=mem_per_worker) as client:
         # dask.set_options(get=dask.local.get_sync)
         dprint(f'Dask Client: {client}', flush=True)
-        # print(f'Dashboard: {client.dashboard_link}', flush=True)
+        dprint(f'Dashboard: {client.dashboard_link}', flush=True)
         dprint(f'Using {n_workers} workers, each with {threads_per_worker} threads, on {nprocs} CPUs.')
 
         #Dask Dataframe of all keys
