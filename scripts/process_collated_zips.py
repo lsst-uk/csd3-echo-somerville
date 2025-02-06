088a43a6e19aa189247e809d5ce6f00a42dbfa2c
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-02-06 14:04:43 +0000

    try more CPUs

--- scripts/process_collated_zips.py
+++ scripts/process_collated_zips.py
@@ -265,7 +265,7 @@ def main():
             nprocs = cpu_count() - 1
     else:
         nprocs = 6
-    threads_per_worker = nprocs
+    threads_per_worker = 1
     n_workers = nprocs//threads_per_worker
     mem_per_worker = 64*1024**3//n_workers # limit to 64 GiB total memory
 
