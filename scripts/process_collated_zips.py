e07b1d9864746aa9a6947053453f21feb260415e
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-02-24 14:25:50 +0000

    debug

--- scripts/process_collated_zips.py
+++ scripts/process_collated_zips.py
@@ -223,6 +223,7 @@ def extract_and_upload(key: str, conn: swiftclient.Connection, bucket_name: str)
             content_file_data.seek(0)
             try:
                 existing_content = conn.head_object(bucket_name,content_key)
+                dprint(content_key, key)
                 dprint(existing_content['etag'], content_md5)
                 if existing_content['etag'] == content_md5:
                     dprint('Content already exists. Skipping.')
