72e5464b19304f313c9c6eee867aba6731cb6e23
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-02-25 16:49:29 +0000

    seek(0) if data can be

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -644,7 +644,8 @@ def upload_to_bucket(s3, bucket_name, api, local_dir, folder, filename, object_k
                 - Create checksum object
                 """
                 checksum_hash = hashlib.md5(file_data)
-                file_data.seek(0)
+                if hasattr(file_data, 'seek'):
+                    file_data.seek(0)
                 checksum_string = checksum_hash.hexdigest()
                 checksum_base64 = base64.b64encode(checksum_hash.digest()).decode()
 
@@ -734,7 +735,8 @@ def upload_to_bucket_collated(s3, bucket_name, api, folder, file_data, zip_conte
 
         filename = object_key.split('/')[-1]
         file_data_size = len(file_data)
-        file_data.seek(0)
+        if hasattr(file_data, 'seek'):
+            file_data.seek(0)
 
         print(f'Uploading zip file "{filename}" for {folder} to {bucket_name}/{object_key}, {file_data_size} bytes, checksum = True, dryrun = {dryrun}', flush=True)
         """
@@ -745,7 +747,8 @@ def upload_to_bucket_collated(s3, bucket_name, api, folder, file_data, zip_conte
             - Create checksum object
             """
             checksum_hash = hashlib.md5(file_data)
-            file_data.seek(0)
+            if hasattr(file_data, 'seek'):
+                file_data.seek(0)
             checksum_string = checksum_hash.hexdigest()
             checksum_base64 = base64.b64encode(checksum_hash.digest()).decode()
 
@@ -785,7 +788,8 @@ def upload_to_bucket_collated(s3, bucket_name, api, folder, file_data, zip_conte
 
         filename = object_key.split('/')[-1]
         file_data_size = len(file_data)
-        file_data.seek(0)
+        if hasattr(file_data, 'seek'):
+            file_data.seek(0)
 
         print(f'Uploading zip file "{filename}" for {folder} to {bucket_name}/{object_key}, {file_data_size} bytes, checksum = True, dryrun = {dryrun}', flush=True)
         """
@@ -796,7 +800,8 @@ def upload_to_bucket_collated(s3, bucket_name, api, folder, file_data, zip_conte
             - Create checksum object
             """
             checksum_hash = hashlib.md5(file_data)
-            file_data.seek(0)
+            if hasattr(file_data, 'seek'):
+                file_data.seek(0)
             checksum_string = checksum_hash.hexdigest()
             checksum_base64 = base64.b64encode(checksum_hash.digest()).decode()
 
