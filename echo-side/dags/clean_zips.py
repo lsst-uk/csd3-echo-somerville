7651a2d817ef2daaf82967934a235fe036548c41
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-03-03 15:43:22 +0000

    add limit to bucket_contents.py

---
+++ echo-side/dags/clean_zips.py
@@ -0,0 +1,74 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from airflow.models.xcom_arg import XComArg
+
+from datetime import timedelta, datetime
+
+
+def dl_bucket_names(url):
+    import json
+    import requests
+    bucket_names = []
+    r = requests.get(url)
+    buckets = json.loads(r.text)
+    for bucket in buckets:
+        bucket_names.append(bucket['name'])
+    print(f'Bucket names found: {bucket_names}')
+    return bucket_names
+
+bucket_names = dl_bucket_names('https://raw.githubusercontent.com/lsst-uk/csd3-echo-somerville/main/echo-side/bucket_names/bucket_names.json')
+
+def print_bucket_name(bucket_name):
+    print(bucket_name)
+
+# Define default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'retries': 0,
+    'retry_delay': timedelta(seconds=60),
+}
+
+# Instantiate the DAG
+
+
+with DAG(
+        'process_zips',
+        default_args=default_args,
+        description='Runs process_collated_zips.py',
+        schedule=timedelta(days=1),
+        start_date=datetime(2024, 1, 1),
+        catchup=False,
+    ) as dag:
+
+    print_bucket_name_task = [
+        PythonOperator(
+            task_id=f'print_bucket_name_{bucket_name}',
+            python_callable=print_bucket_name,
+            op_kwargs={'bucket_name': bucket_name},
+        ) for bucket_name in bucket_names]
+
+    # if len(bucket_names) > 0:
+    #     print(f'Bucket names found: {bucket_names}')
+    process_zips_task = [
+        KubernetesPodOperator(
+        task_id=f'process_zips_{bucket_name}',
+        image='ghcr.io/lsst-uk/csd3-echo-somerville:latest',
+        cmds=['/entrypoint.sh'],
+        arguments=['python', 'csd3-echo-somerville/scripts/process_collated_zips.py', '--bucket-name', bucket_name, '--extract', '--nprocs', '24'],
+        env_vars={
+            'S3_ACCESS_KEY': Variable.get("S3_ACCESS_KEY"),
+            'S3_SECRET_KEY': Variable.get("S3_SECRET_KEY"),
+            'S3_HOST_URL': Variable.get("S3_HOST_URL"),
+            'ST_AUTH': Variable.get("ST_AUTH"),
+            'ST_USER': Variable.get("ST_USER"),
+            'ST_KEY': Variable.get("ST_KEY"),
+        },
+        get_logs=True,
+    ) for bucket_name in bucket_names]
+    # else:
+    #     print('No bucket names found.')
+
+    print_bucket_name_task
+    process_zips_task
--- scripts/bucket_contents.py
+++ scripts/bucket_contents.py
@@ -7,8 +7,9 @@ import sys
 import os
 import bucket_manager.bucket_manager as bm
 
-def list_all(bucket):
+def list_all(bucket,limit):
     total_size = 0
+    count = 0
     for ob in bucket.objects.all():
         if ob.key.endswith('zip'):
             try:
@@ -18,10 +19,14 @@ def list_all(bucket):
         else:
             print(f'{ob.key}, {ob.size/1024**2:.2f}, {ob.last_modified}, n/a')
         total_size += ob.size
+        count += 1
+        if count == limit:
+            break
     return total_size
 
-def list_prefix(bucket,prefix):
+def list_prefix(bucket,prefix,limit):
     total_size = 0
+    count = 0
     for ob in bucket.objects.filter(Prefix=prefix):
         if ob.key.endswith('zip'):
             try:
@@ -31,6 +36,9 @@ def list_prefix(bucket,prefix):
         else:
             print(f'{ob.key}, {ob.size/1024**2:.2f}, {ob.last_modified}, n/a')
         total_size += ob.size
+        count += 1
+        if count == limit:
+            break
     return total_size
 
 if __name__ == '__main__':
@@ -38,10 +46,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Measure transfer speed to an S3 bucket.')
     parser.add_argument('--bucket-name', '-b', type=str, help='Name of the S3 bucket', required=True)
     parser.add_argument('--prefix', '-p', type=str, default='', help='Optional prefix to filter objects in the bucket')
+    parser.add_argument('--limit', '-l', type=int, default=0, help='Optional limit on the number of objects to list')
 
     args = parser.parse_args()
     bucket_name = args.bucket_name
     prefix = args.prefix
+    limit = args.limit
 
     try:
         assert bm.check_keys()
@@ -60,9 +70,9 @@ if __name__ == '__main__':
         print('Contents of bucket: ',bucket_name)
         print('Object, Size / MiB, LastModified, Zip Contents')
         if prefix:
-            total_size = list_prefix(bucket,prefix)
+            total_size = list_prefix(bucket,prefix,limit)
         else:
-            total_size = list_all(bucket)
+            total_size = list_all(bucket,limit)
     except Exception as e:
         if '(NoSuchBucket)' in str(e).split():
             print('NoSuchBucket')
