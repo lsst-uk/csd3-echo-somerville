a2ac0409e6e85cfbc78933a14378487d933b7057
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-03-14 11:31:16 +0000

    debugging to_collate dataframe

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1658,6 +1658,8 @@ def process_files(
                     True for _ in range(len(individual_files))
                 ]
             })
+            to_collate.to_csv('temp.csv', index=False)
+            exit()
             to_collate = dd.from_pandas(to_collate, npartitions=len(client.scheduler_info()['workers']) * 2)
             to_collate[to_collate['type' == 'file']]['upload'] = True
             to_collate[to_collate['type' == 'zip']]['upload'] = to_collate[to_collate['type' == 'zip']]['object_names'].apply(
