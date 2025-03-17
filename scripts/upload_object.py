350b759675a6e83cdbf09845925a0eab6de1895f
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-03-17 13:18:55 +0000

    done (#114)

--- scripts/upload_object.py
+++ scripts/upload_object.py
@@ -7,48 +7,102 @@ This script allows you to upload a single file given bucket name, object_name an
 """
 
 import sys
+import os
 import warnings
 import argparse
+import datetime as dt
 
 import bucket_manager.bucket_manager as bm
 
+
+def upload_file(connection, bucket_name, object_name, local_path, timings=False):
+    """
+    Upload a file to an S3 bucket.
+    Optionally print timings for data loading and upload.
+    """
+    if timings:
+        read_start = dt.datetime.now()
+        file_data = open(local_path, 'rb').read()
+        read_end = dt.datetime.now()
+        size = os.path.getsize(local_path)
+    else:
+        file_data = open(local_path, 'rb').read()
+    try:
+        if timings:
+            upload_start = dt.datetime.now()
+        response = connection.put_object(
+            bucket=bucket_name,
+            obj=object_name,
+            contents=file_data
+        )
+        if timings:
+            upload_end = dt.datetime.now()
+    except Exception as e:
+        print(f'Error {e}', file=sys.stderr)
+        sys.exit()
+    if timings:
+        end = dt.datetime.now()
+        print(f'File size: {size} bytes.')
+        print(f'Read time: {(read_end - read_start).total_seconds()} seconds.')
+        print(f'Upload time: {(upload_end - upload_start).total_seconds()} seconds.')
+        print(f'Transfer speed: {size / (upload_end - upload_start).total_seconds()} bytes per second.')
+        print(f'Total time: {(end - read_start).total_seconds()} seconds.')
+    return response
 
 
 parser = argparse.ArgumentParser(description='Upload a file to an S3 bucket.')
 parser.add_argument('--bucket-name', '-b', type=str, help='The name of the S3 bucket.')
 parser.add_argument('--object-name', '-o', type=str, help='The name of the object to upload.')
 parser.add_argument('--local-path', '-p', type=str, help='The local path to the file to upload.')
+parser.add_argument('--timings', '-t', action='store_true', help='Timings for data loading and upload.')
+parser.add_argument('--api', type=str, help='The API to use for the upload.')
 
 args = parser.parse_args()
 
 bucket_name = args.bucket_name
 object_name = args.object_name
 local_path = args.local_path
+timings = args.timings
+api = args.api.lower()
 
-s3_host = 'echo.stfc.ac.uk'
+if api == 's3':
+    sys.exit('S3 API not yet implemented.')
+elif api == 'swift':
+    pass
+else:
+    sys.exit('API not recognised.')
+
 try:
-    keys = bm.get_keys()
-except KeyError as e:
-    print(e)
+    if bm.check_keys(api):
+        if api == 's3':
+            access_key = os.environ['S3_ACCESS_KEY']
+            secret_key = os.environ['S3_ACCESS_KEY']
+            s3_host = os.environ['S3_HOST_URL']
+        elif api == 'swift':
+            access_key = os.environ['ST_USER']
+            secret_key = os.environ['ST_KEY']
+            s3_host = os.environ['ST_AUTH']
+except AssertionError as e:
+    print(f'AssertionError {e}', file=sys.stderr)
     sys.exit()
-access_key = keys['access_key']
-secret_key = keys['secret_key']
+except KeyError as e:
+    print(f'KeyError {e}', file=sys.stderr)
+    sys.exit()
+except ValueError as e:
+    print(f'ValueError {e}', file=sys.stderr)
+    sys.exit()
 
 warnings.filterwarnings('ignore')
 
-s3 = bm.get_resource(access_key, secret_key, s3_host)
+connection = bm.get_conn_swift(s3_host, access_key, secret_key)
 
-if not bucket_name in bm.bucket_list(s3):
+if bucket_name not in bm.bucket_list_swift(connection):
     sys.exit('Bucket does not exist.')
 
-bucket = s3.Bucket(bucket_name)
-
-# if not object_name in bm.object_list(bucket):
-#     sys.exit('Object does not exist.')
-
-
-response = bucket.upload_file(local_path, object_name) 
-if response:
-    print(response)
-else:
-    print('Upload successful.')
+response = upload_file(
+    connection,
+    bucket_name,
+    object_name,
+    local_path,
+    timings
+)
