2a685922861d05d008ed9a6f3c3cb8c4e5c3b3fb
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-03-21 09:43:54 +0000

    debug log_string

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -796,7 +796,7 @@ def upload_to_bucket(
 
         # for no zip contents
         log_string += ',"n/a"'
-
+        dprint(f'log_string: {log_string}', flush=True)
         with open(log, 'a') as f:
             f.write(log_string + '\n')
 
@@ -1114,7 +1114,7 @@ def upload_to_bucket_collated(
                     break
         with open(log, 'a') as f:
             f.write(log_string + '\n')
-
+        dprint(f'log_string: {log_string}', flush=True)
         return True
 
 
