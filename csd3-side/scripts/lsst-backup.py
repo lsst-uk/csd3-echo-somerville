51eec1fb44aad5a2ec2971c9ae13975c76994508
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-03-26 14:37:30 +0000

    avoid recursion limit

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1470,6 +1470,7 @@ def process_files(
     done_first = False
     print(f'Analysing local dataset {local_dir}.', flush=True)
     for folder, sub_folders, files in os.walk(local_dir, topdown=True):
+        print(f'in {folder}', flush=True)
         if exclude.isin([folder]).any():
             continue
         if len(files) == 0 and len(sub_folders) == 0:
@@ -1500,8 +1501,10 @@ def process_files(
                 ]
             )
         total_all_folders += 1
-        if total_all_folders % 1000 == 0:
-            print('.', end='', flush=True)
+        if total_all_folders % 900 == 0:
+            # avoid hitting the recursion limit
+            ddf = ddf.compute().reset_index(drop=True)
+            ddf = dd.from_pandas(ddf)
 
         done_first = True
     print()
