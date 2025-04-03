9a74846ce411962585098fb3392968df393bc34c
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-04-03 15:35:11 +0100

    .

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1693,7 +1693,7 @@ def process_files(
         # to_collate
         list_aggregation = dd.Aggregation(
             'list_aggregation',
-            lambda li: print(li),  # chunks are aggregated into strings with str.join('|')
+            lambda li: print(li.values),  # chunks are aggregated into strings with str.join('|')
             lambda li: print(li),  # strings are aggregated into a single string with str.join('|')
             lambda x: x.values        # finalize by settings the values of the generated Series
         )
