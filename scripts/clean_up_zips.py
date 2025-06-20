d36e484ab82fb6b232b66badc816d4b9273bcde1
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-06-20 16:08:07 +0100

    try more partitions

--- scripts/clean_up_zips.py
+++ scripts/clean_up_zips.py
@@ -486,7 +486,7 @@ if __name__ == '__main__':
         len_co = len(current_object_names)
         current_objects = dd.from_pandas(
             current_object_names,
-            npartitions=n_workers * 100
+            npartitions=n_workers * 1000
         )
         # del current_object_names
         # nparts = current_objects.npartitions
