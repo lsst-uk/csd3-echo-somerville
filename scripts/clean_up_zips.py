6d747128d7b4c68edf7aaf80a37a6ae5fd04c59b
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-06-26 16:02:34 +0100

    add tag to namespace

--- scripts/clean_up_zips.py
+++ scripts/clean_up_zips.py
@@ -491,7 +491,7 @@ if __name__ == '__main__':
     tag = ''
     for i in range(6):
         tag += letters[randint(0, 25)]
-    namespace = 'lsstuk-dask'
+    namespace = 'lsstuk-dask-' + tag
     logprint(f'Using namespace: {namespace}', 'info')
     os.system(f'kubectl create namespace {namespace}')
     cluster = KubeCluster(
