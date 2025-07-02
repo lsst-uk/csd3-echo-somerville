9380c3937e785449030f6eb0f8d8c2cfafb35a33
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-07-02 14:34:52 +0100

    reduce mem

--- scripts/clean_up_zips.py
+++ scripts/clean_up_zips.py
@@ -549,11 +549,11 @@ if __name__ == '__main__':
         n_workers=dask_workers,
         resources={
             "requests": {
-                "memory": '80Gi',  # f'{mem_request}Gi',
+                "memory": '64Gi',  # f'{mem_request}Gi',
                 "cpu": '8'  # f'{cpus_per_worker}'
             },
             "limits": {
-                "memory": '160Gi',  # f'{mem_limit}Gi',
+                "memory": '96Gi',  # f'{mem_limit}Gi',
                 "cpu": '16'  # f'{max_cpus_per_worker}'
             }
         },
