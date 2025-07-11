708fe7021e8f19947ac7108761cf0376c47cbf3a
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-07-11 14:32:53 +0100

    minor

--- csd3-side/scripts/lsst-backup.py
+++ csd3-side/scripts/lsst-backup.py
@@ -1489,9 +1489,9 @@ def process_files(
                 batch_id += 1
                 cumulative_size = 0
             batch_assignments.append(batch_id)
-            print(f'{len(batch_assignments)}, ', end='\r', flush=True)
+            print(f'Row {len(batch_assignments)}', end='\r', flush=True)
             cumulative_size += row['size']
-        print('', flush=True)
+        print()
         zip_files_ddf['id'] = batch_assignments
 
     # 5. Prepare Dask DataFrames for upload
